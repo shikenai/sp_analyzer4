c070a4f6c458f31f06278fef3537fff82712a46c
shikenai <kakehashi.y2788@gmail.com>
2023-08-11 13:09:57 +0900

    apiのURL設定を確認した

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+import os
+import pandas as pd
+from django.http import JsonResponse
+import json
+
+
+def test(request):
+    return JsonResponse({"key": "balue"})
